d45fee0f8fe0f8a082024adaff5786223133f410
Jack Wetherell <jack.wetherell@gmail.com>
2022-01-24 20:26:10 +0000

    Updated __init__.

--- src/iDEA/__init__.py
+++ src/iDEA/__init__.py
@@ -1,9 +1,10 @@
 import iDEA.system
 import iDEA.interactions
+import iDEA.state
 import iDEA.utilities
 
 
-__all__ = ['iDEA.system', 'iDEA.utilities', 'iDEA.interactions']
+__all__ = ['iDEA.system', 'iDEA.interactions', 'iDEA.state', 'iDEA.utilities']
 
 
 del iDEA
