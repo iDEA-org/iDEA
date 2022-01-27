92460e9b8fe9c9443e2c3a9a741d9f4a9621a710
Jack Wetherell <jack.wetherell@gmail.com>
2022-01-27 15:20:33 +0000

    Updated __init__ with new modules.

--- src/iDEA/__init__.py
+++ src/iDEA/__init__.py
@@ -2,9 +2,10 @@ import iDEA.system
 import iDEA.interactions
 import iDEA.state
 import iDEA.utilities
+import iDEA.non_interacting
 
 
-__all__ = ['iDEA.system', 'iDEA.interactions', 'iDEA.state', 'iDEA.utilities']
+__all__ = ["iDEA.utilities", "iDEA.system", "iDEA.interactions", "iDEA.state", "iDEA.non_interacting"]
 
 
 del iDEA
