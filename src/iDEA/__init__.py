94292dd64d11e4126357a1fa9ef7e22d37c62fbe
Jack Wetherell <jack.wetherell@gmail.com>
2022-03-04 22:40:48 +0000

    Added interacting import.

--- src/iDEA/__init__.py
+++ src/iDEA/__init__.py
@@ -2,11 +2,12 @@ import iDEA.utilities
 import iDEA.system
 import iDEA.interactions
 import iDEA.state
-import iDEA.observables
+import iDEA.observables#
+import iDEA.methods.interacting
 import iDEA.methods.non_interacting
 
 
-__all__ = ["iDEA.utilities", "iDEA.system", "iDEA.interactions", "iDEA.state", "iDEA.observables" ,"iDEA.methods.non_interacting"]
+__all__ = ["iDEA.utilities", "iDEA.system", "iDEA.interactions", "iDEA.state", "iDEA.observables" ,"iDEA.methods.interacting" ,"iDEA.methods.non_interacting"]
 
 
 del iDEA
