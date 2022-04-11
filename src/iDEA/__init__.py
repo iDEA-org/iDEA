c3980db9c20bed843cd683b52c06c5cac83c4400
Jack Wetherell <jack.wetherell@gmail.com>
2022-04-11 20:30:11 +0000

    Added iterable modules.

--- src/iDEA/__init__.py
+++ src/iDEA/__init__.py
@@ -2,9 +2,14 @@ import iDEA.utilities
 import iDEA.system
 import iDEA.interactions
 import iDEA.state
-import iDEA.observables#
+import iDEA.observables
 import iDEA.methods.interacting
 import iDEA.methods.non_interacting
+import iDEA.methods.hartree
+import iDEA.methods.hartree_fock
+ 
+
+__all__ = ["iDEA.utilities", "iDEA.system", "iDEA.interactions", "iDEA.state", "iDEA.observables", "iDEA.methods.interacting" ,"iDEA.methods.non_interacting", "iDEA.methods.hartree", "iDEA.methods.hartree_fock"]
 
 
-__all__ = ["iDEA.utilities", "iDEA.system", "iDEA.interactions", "iDEA.state", "iDEA.observables" ,"iDEA.methods.interacting" ,"iDEA.methods.non_interacting"]
+iterate_methods = [iDEA.methods.interacting, iDEA.methods.non_interacting, iDEA.methods.hartree_fock]
