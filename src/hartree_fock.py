258a0793becac75cf9a9ad708fa758e180d6e2eb
Jack Wetherell <jack.wetherell@gmail.com>
2022-04-20 18:43:31 +0000

    Added time-dependence.

--- src/hartree_fock.py
+++ src/hartree_fock.py
@@ -8,7 +8,7 @@ import numpy as np # Python's numerical library
 from numpy import diag
 import scipy.integrate as spi
 from scipy.linalg import eigh # To solve the ground-state Schroedinger equation (eigenproblem)
-# from scipy.linalg import expm # For time propagation
+from scipy.linalg import expm # For time propagation
 import matplotlib.pyplot as plt # For plotting
 from scipy.sparse import spdiags
 
@@ -28,8 +28,8 @@ def Hamiltonian( spin, perturbing_potential, Kinetic_energy, Externa_potential, 
     Fock_operator = fock( occ, psi, coulomb_potential )
     if time_dependent == False:
         return Kinetic_energy + Externa_potential + Hartree_potential + ( fock_on * Fock_operator * dx ) # Full Hamiltonian
-    # elif time_dependent == True:
-    #     return Kinetic_energy + Externa_potential + Hartree_potential + ( fock_on * Fock_operator * dx ) + perturbing_potential # Add perturbing potential to the Hamiltonian
+    elif time_dependent == True:
+        return Kinetic_energy + Externa_potential + Hartree_potential + ( fock_on * Fock_operator * dx ) + perturbing_potential # Add perturbing potential to the Hamiltonian
 
 def density( psi, occ ): # Calculate the electron density
     den = np.zeros( psi.shape[0], dtype = "float" )
@@ -74,17 +74,17 @@ def solve( mixing, Kinetic_energy, Externa_potential, occ_channel_1, occ_channel
         psi_old_channel_2 = psi_new_channel_2
     return psi_new_channel_1, psi_new_channel_2, eigenvalues_channel_1, eigenvalues_channel_2
 
-# def timestep( spin, wavefunctions_channel_1, wavefunctions_channel_2, Kinetic_energy, Externa_potential, perturbing_potential, occ_channel_1, occ_channel_2, dx, dt, fock_on ):
-#     if spin == 'up':
-#         occ = occ_channel_1
-#         wavefunctions = wavefunctions_channel_1
-#     else:
-#         occ = occ_channel_2
-#         wavefunctions = wavefunctions_channel_2
-#     for i in range( len(occ) ):
-#         wavefunctions[:,i] = np.dot(expm(-1.0j*dt*Hamiltonian( spin, perturbing_potential, Kinetic_energy, Externa_potential, occ_channel_1, occ_channel_2, coulomb_potential, potential, wavefunctions_channel_1, wavefunctions_channel_2, spatial_points, dx, fock_on, True )), wavefunctions[:,i]) # Propagate the wavefunction through one time step
-#         #wavefunctions[:,i] /= np.sqrt( np.trapz( abs( wavefunctions[:,i] )**2, dx = dx, axis = 0 ) ) # spatial_pointsormalise the wavefunction
-#     return wavefunctions
+def timestep( spin, wavefunctions_channel_1, wavefunctions_channel_2, Kinetic_energy, Externa_potential, perturbing_potential, occ_channel_1, occ_channel_2, dx, dt, fock_on ):
+    if spin == 'up':
+        occ = occ_channel_1
+        wavefunctions = wavefunctions_channel_1
+    else:
+        occ = occ_channel_2
+        wavefunctions = wavefunctions_channel_2
+    for i in range( len(occ) ):
+        wavefunctions[:,i] = np.dot(expm(-1.0j*dt*Hamiltonian( spin, perturbing_potential, Kinetic_energy, Externa_potential, occ_channel_1, occ_channel_2, coulomb_potential, potential, wavefunctions_channel_1, wavefunctions_channel_2, spatial_points, dx, fock_on, True )), wavefunctions[:,i]) # Propagate the wavefunction through one time step
+        #wavefunctions[:,i] /= np.sqrt( np.trapz( abs( wavefunctions[:,i] )**2, dx = dx, axis = 0 ) ) # spatial_pointsormalise the wavefunction
+    return wavefunctions
 
 def occupations( spins, restricted ):
     Number_electrons_up = spins.count( 'u' )
@@ -132,17 +132,14 @@ def total_energy( channel_1, channel_2, wavefunctions_channel_1, wavefunctions_c
 
 #################################################################################################################################################
 
-L = 20.0 # Length of the real-space grid
-dx = 0.1 # Grid spacing
+L = 40.0 # Length of the real-space grid
+dx = 0.2 # Grid spacing
 spatial_points = int( L / dx ) # Number of grid points
 
 spins = 'ud'
 restricted = 'True'
 
 channel_1, channel_2 = occupations( spins, restricted )
-
-print(channel_1, channel_2)
-raise OSError()
 
 sorted_channel_1 = channel_1.copy()
 sorted_channel_1.sort( reverse=True )
@@ -156,7 +153,7 @@ potential = 0.5*( 0.25**2 )*x**2 #-1.0/(abs(x+5)+1) -1.0/(abs(x-5)+1) -3.0/(abs(
 Kinetic_energy = spdiags( np.array( [ (1.0/dx**2)*np.ones(spatial_points), (-0.5/dx**2)*np.ones(spatial_points), (-0.5/dx**2)*np.ones(spatial_points)] ), np.array( [0,-1,1] ), spatial_points, spatial_points ) # Kinetic energy
 Externa_potential = diag( potential ) # Potential energy
 coulomb_potential = coulomb(x) # Calculate once and not again
-perturbing_potential = diag( -0.05*x )
+perturbing_potential = diag( -0.1*x )
 psi_start_channel_1 = np.zeros( (spatial_points, len(channel_1) ) )
 psi_start_channel_2 = np.zeros( (spatial_points, len(channel_2) ) )
 non_interaction_wavefunction_channel_1, non_interaction_wavefunction_channel_2, non_interaction_eigenenergies_channel_1, non_interaction_eigenenergies_channel_2 = solve( 0.5, Kinetic_energy, Externa_potential, sorted_channel_1, sorted_channel_2, (0*coulomb_potential), potential, spatial_points, dx, psi_start_channel_1, psi_start_channel_2, 0 )
@@ -182,39 +179,39 @@ plt.show()
 
 ############################################################# Time dependence #################################################################
 
-# number_timesteps = 500
-# simulation_time = 10.
-# dt = simulation_time / number_timesteps
-# density_array = np.zeros( (number_timesteps,spatial_points), dtype='float' )
-# time_dependent_wavefunctions_channel_1 = np.zeros( (number_timesteps, spatial_points, len(sorted_channel_1) ), dtype = "complex")
-# time_dependent_wavefunctions_channel_2 = np.zeros( (number_timesteps, spatial_points, len(sorted_channel_1) ), dtype = "complex")
-# time_dependent_wavefunctions_channel_1[0,...] = hartree_fock_wavefunction_channel_1 # Initialise the wavefunction
-# time_dependent_wavefunctions_channel_2[0,...] = hartree_fock_wavefunction_channel_2 # Initialise the wavefunction
-# for j in range( number_timesteps-1 ):
-#     print('Simulation time %.2f atomic units' % ( j*dt ), end='\r')
-#     time_dependent_wavefunctions_channel_1[j+1,...] = timestep( 'up', time_dependent_wavefunctions_channel_1[j,...], time_dependent_wavefunctions_channel_2[j,...], Kinetic_energy, Externa_potential, perturbing_potential, sorted_channel_1, sorted_channel_2, dx, dt, 1 )
-#     time_dependent_wavefunctions_channel_2[j+1,...] = timestep( 'down', time_dependent_wavefunctions_channel_1[j,...], time_dependent_wavefunctions_channel_2[j,...], Kinetic_energy, Externa_potential, perturbing_potential, sorted_channel_1, sorted_channel_2, dx, dt, 1 )
-#     density_array[j,:] = density( time_dependent_wavefunctions_channel_1[j,...], channel_1 ) + density( time_dependent_wavefunctions_channel_2[j,...], channel_2 )
-#     if ( j * 5 ) % number_timesteps < 5: # Plot sample timesteps throughout the whole simulation
-#         plt.plot( x, ( density( time_dependent_wavefunctions_channel_1[j,...], channel_1 ) + density( time_dependent_wavefunctions_channel_2[j,...], channel_2 ) ), label = r'$t = %.2f$' % ( j*dt ) )
+number_timesteps = 800
+simulation_time = 200.
+dt = simulation_time / number_timesteps
+density_array = np.zeros( (number_timesteps,spatial_points), dtype='float' )
+time_dependent_wavefunctions_channel_1 = np.zeros( (number_timesteps, spatial_points, len(sorted_channel_1) ), dtype = "complex")
+time_dependent_wavefunctions_channel_2 = np.zeros( (number_timesteps, spatial_points, len(sorted_channel_1) ), dtype = "complex")
+time_dependent_wavefunctions_channel_1[0,...] = hartree_fock_wavefunction_channel_1 # Initialise the wavefunction
+time_dependent_wavefunctions_channel_2[0,...] = hartree_fock_wavefunction_channel_2 # Initialise the wavefunction
+for j in range( number_timesteps-1 ):
+    print('Simulation time %.2f atomic units' % ( j*dt ), end='\r')
+    time_dependent_wavefunctions_channel_1[j+1,...] = timestep( 'up', time_dependent_wavefunctions_channel_1[j,...], time_dependent_wavefunctions_channel_2[j,...], Kinetic_energy, Externa_potential, perturbing_potential, sorted_channel_1, sorted_channel_2, dx, dt, 1 )
+    time_dependent_wavefunctions_channel_2[j+1,...] = timestep( 'down', time_dependent_wavefunctions_channel_1[j,...], time_dependent_wavefunctions_channel_2[j,...], Kinetic_energy, Externa_potential, perturbing_potential, sorted_channel_1, sorted_channel_2, dx, dt, 1 )
+    density_array[j,:] = density( time_dependent_wavefunctions_channel_1[j,...], channel_1 ) + density( time_dependent_wavefunctions_channel_2[j,...], channel_2 )
+    if ( j * 5 ) % number_timesteps < 5: # Plot sample timesteps throughout the whole simulation
+        plt.plot( x, ( density( time_dependent_wavefunctions_channel_1[j,...], channel_1 ) + density( time_dependent_wavefunctions_channel_2[j,...], channel_2 ) ), label = r'$t = %.2f$' % ( j*dt ) )
 
-# plt.legend( prop = {"size":11}, frameon = True, ncol = 3 , loc = 'upper center', bbox_to_anchor = ( 0.5, 1.3 ), fancybox = True )
-# plt.ylabel( r'$n(x,t)$' )
-# plt.xlabel( r'$x$' )
-# plt.tight_layout()
-# plt.show()
+plt.legend( prop = {"size":11}, frameon = True, ncol = 3 , loc = 'upper center', bbox_to_anchor = ( 0.5, 1.3 ), fancybox = True )
+plt.ylabel( r'$n(x,t)$' )
+plt.xlabel( r'$x$' )
+plt.tight_layout()
+plt.show()
 
-# current = calculate_current_density(density_array, dt, dx)
+current = calculate_current_density(density_array, dt, dx)
 
-# for j in range( number_timesteps-1 ):
-#     if ( j * 5 ) % number_timesteps < 5:
-#         plt.plot( ( current[j,:] ), label = r'$t = %.2f$' % ( j*dt ) )
+for j in range( number_timesteps-1 ):
+    if ( j * 5 ) % number_timesteps < 5:
+        plt.plot( ( current[j,:] ), label = r'$t = %.2f$' % ( j*dt ) )
 
-# plt.legend( prop = {"size":11}, frameon = True, ncol = 3 , loc = 'upper center', bbox_to_anchor = ( 0.5, 1.3 ), fancybox = True )
-# plt.ylabel( r'$j(x,t)$' )
-# plt.xlabel( r'$x$' )
-# plt.tight_layout()
-# plt.show()
+plt.legend( prop = {"size":11}, frameon = True, ncol = 3 , loc = 'upper center', bbox_to_anchor = ( 0.5, 1.3 ), fancybox = True )
+plt.ylabel( r'$j(x,t)$' )
+plt.xlabel( r'$x$' )
+plt.tight_layout()
+plt.show()
 
 if restricted == 'True':
     channel_2 = [0] * len(channel_1)
