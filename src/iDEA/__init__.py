19a98308885ddf8024af21904be9e6cafbd84560
Jack Wetherell <jack.wetherell@gmail.com>
2022-05-24 23:01:00 +0000

    Added hybrid code.

--- src/iDEA/__init__.py
+++ src/iDEA/__init__.py
@@ -8,11 +8,12 @@ import iDEA.methods.non_interacting
 import iDEA.methods.hartree
 import iDEA.methods.hartree_fock
 import iDEA.methods.lda
+import iDEA.methods.hybrid
  
 
-__all__ = ["iDEA.utilities", "iDEA.system", "iDEA.interactions", "iDEA.state", "iDEA.observables", "iDEA.methods.interacting" ,"iDEA.methods.non_interacting", "iDEA.methods.hartree", "iDEA.methods.hartree_fock", "iDEA.methods.lda"]
+__all__ = ["iDEA.utilities", "iDEA.system", "iDEA.interactions", "iDEA.state", "iDEA.observables", "iDEA.methods.interacting" ,"iDEA.methods.non_interacting", "iDEA.methods.hartree", "iDEA.methods.hartree_fock", "iDEA.methods.lda", "iDEA.methods.hybrid"]
 
 
-iterate_methods = [iDEA.methods.interacting, iDEA.methods.non_interacting, iDEA.methods.hartree, iDEA.methods.hartree_fock, iDEA.methods.lda]
+iterate_methods = [iDEA.methods.interacting, iDEA.methods.non_interacting, iDEA.methods.hartree, iDEA.methods.hartree_fock, iDEA.methods.lda, iDEA.methods.hybrid]
 iterate_mb_methods = [iDEA.methods.interacting]
-iterate_sb_methods = [iDEA.methods.non_interacting, iDEA.methods.hartree, iDEA.methods.hartree_fock, iDEA.methods.lda]
+iterate_sb_methods = [iDEA.methods.non_interacting, iDEA.methods.hartree, iDEA.methods.hartree_fock, iDEA.methods.lda, iDEA.methods.hybrid]
