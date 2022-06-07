ae86ceac7a365821a8c80d6d536c985aa47d27a8
Jack Wetherell <jack.wetherell@gmail.com>
2022-06-07 20:47:14 +0000

    Added reverse engineering.

--- src/iDEA/__init__.py
+++ src/iDEA/__init__.py
@@ -9,9 +9,10 @@ import iDEA.methods.hartree
 import iDEA.methods.hartree_fock
 import iDEA.methods.lda
 import iDEA.methods.hybrid
- 
+import iDEA.reverse 
 
-__all__ = ["iDEA.utilities", "iDEA.system", "iDEA.interactions", "iDEA.state", "iDEA.observables", "iDEA.methods.interacting" ,"iDEA.methods.non_interacting", "iDEA.methods.hartree", "iDEA.methods.hartree_fock", "iDEA.methods.lda", "iDEA.methods.hybrid"]
+
+__all__ = ["iDEA.utilities", "iDEA.system", "iDEA.interactions", "iDEA.state", "iDEA.observables", "iDEA.methods.interacting" ,"iDEA.methods.non_interacting", "iDEA.methods.hartree", "iDEA.methods.hartree_fock", "iDEA.methods.lda", "iDEA.methods.hybrid", "iDEA.reverse"]
 
 
 iterate_methods = [iDEA.methods.interacting, iDEA.methods.non_interacting, iDEA.methods.hartree, iDEA.methods.hartree_fock, iDEA.methods.lda, iDEA.methods.hybrid]
