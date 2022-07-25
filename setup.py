390326eeff15365a636ce14e36f0572ab7307261
Jack Wetherell <jack.wetherell@gmail.com>
2022-07-25 17:07:33 +0100

    Code refactor,

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="iDEA-latest",
-    version="0.1.2",
+    version="0.1.3",
     author="Jack Wetherell",
     author_email="jack.wetherell@gmail.com",
     description="interacting Dynamic Electrons Approach (iDEA)",
@@ -19,18 +19,20 @@ setuptools.setup(
     },
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: GPL License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
+    package_dir={"": "iDEA"},
+    packages=setuptools.find_packages(where="iDEA"),
     python_requires=">=3.8",
     install_requires=[
         'numpy>=1.22.3',
         'scipy>=1.8.0',
         'matplotlib>=3.5.1',
         'jupyterlab>=3.3.2',
+        'tqdm>=4.64.0',
         'black>=22.3.0',
+        'autoflake>=1.4.0',
         'build>=0.7.0',
         'twine>=4.0.0',
     ],
